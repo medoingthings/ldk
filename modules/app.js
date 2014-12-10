317855b379a1a4cdaddbbfeddb965cfeb554a1ab
Mark Rickert <mark@medoingthings.com>
2014-12-10 21:46:25 +0100

    Introduce fastclick Update package.json

--- modules/app.js
+++ modules/app.js
@@ -2,8 +2,12 @@
  * Initialize scripts via browserify
  */
 
+var attachFastClick = require('fastclick');
 var gallery = require('./gallery/gallery');
 var navigationMain = require('./navigation/navigation-main');
+
+// Remove 300ms delay for clicks on touch devices
+attachFastClick(document.body);
 
 navigationMain.init();
 gallery.init();
